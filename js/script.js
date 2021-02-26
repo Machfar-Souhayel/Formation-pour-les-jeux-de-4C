98edbfc970bc762aaa6456dcf7de67ab84fab683
Machfar <souhayel.isetch@gmail.com>
2021-02-27 00:27:03 +0100

    Mohamed Souhayel Machfar

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function changeColorToGreen() {
+    document.documentElement.style.setProperty('--color-blue', '#18bd9b');
+}
+
+function changeColorToOrange() {
+    document.documentElement.style.setProperty('--color-blue', '#f05454');
+}
+
+function changeColorToDark() {
+    document.documentElement.style.setProperty('--color-blue', '#2a5172');
+}
